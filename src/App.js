8cf14124089ffd20cb09452fb2ff30d14857fef4
VanHau87 <nguyenvanh29@gmail.com>
2022-05-25 23:13:49 +0700

    Set up project and call api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import styles from "./App.module.css";
+import { Cards, Chart, CountryPicker } from "./components";
 function App() {
-  return <div>App</div>;
+  return (
+    <div className={styles.container}>
+      <Cards />
+      <Chart />
+      <CountryPicker />
+    </div>
+  );
 }
 
 export default App;
